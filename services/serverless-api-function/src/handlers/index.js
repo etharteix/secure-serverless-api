46544ac1564ef5d1c1924d3752742d324717a913
etharteix <59590437+etharteix@users.noreply.github.com>
2023-10-02 21:24:56 -0300

    Updating serverless api function

--- services/serverless-api-function/src/handlers/index.js
+++ services/serverless-api-function/src/handlers/index.js
@@ -2,8 +2,16 @@ const serverless = require('serverless-http')
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+app.get('/path1', (req, res) => {
+    res.send('GET PATH 1')
+})
+
+app.post('/path1', (req, res) => {
+    res.send('POST PATH 1')
+})
+
+app.get('/path2', (req, res) => {
+    res.send('GET PATH 2')
 })
 
 module.exports.handler = serverless(app)
