334880754807612ad65deb13a838c6e747b49da8
etharteix <59590437+etharteix@users.noreply.github.com>
2023-10-06 12:53:21 -0300

    Adding comments on Lambda resource

--- services/cloudfront-api/src/helpers/validator.js
+++ services/cloudfront-api/src/helpers/validator.js
@@ -1,7 +1,7 @@
 const API_KEY = process.env.API_KEY
 
 const validateRoute = (req) => {
-  const requestApiKey = req.apiGateway.event.headers[`x-api-key`]
+  const requestApiKey = req.apiGateway.event.headers['x-api-key']
 
   return requestApiKey === API_KEY
 }
