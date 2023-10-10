5ed7ee92ef587f8003ce21f25a5af03c688cc66e
etharteix <59590437+etharteix@users.noreply.github.com>
2023-10-10 13:51:37 -0300

    Making the Lambda Function simpler

--- cloudfront-api/services/cloudfront-api/src/handlers/index.js
+++ cloudfront-api/services/cloudfront-api/src/handlers/index.js
@@ -1,33 +1,18 @@
 const serverless = require('serverless-http')
 const express = require('express')
 
-const validator = require('../helpers/validator')
-const customLogger = require('../helpers/custom-logger')
-const constants = require('../helpers/constants')
-
-const path2 = require('../routes/path2')
-
 const app = express()
 
 app.get('/path1', (req, res) => {
-  const queryParams = req.query
-
-  customLogger.info(req, queryParams)
-  res.status(200).send(constants.successMessage)
+  res.send('GET PATH 1')
 })
 
 app.post('/path1', (req, res) => {
-  if (!validator.validateRoute(req)) {
-    customLogger.error(req, constants.invalidApiKeyMessage)
-    res.status(403).send(constants.forbiddenMessage)
-  }
-
-  const body = JSON.parse(req.apiGateway.event.body)
-
-  customLogger.info(req, body)
-  res.status(200).send(constants.successMessage)
+  res.send('POST PATH 1')
 })
 
-app.get('/path2', path2.get)
+app.get('/path2', (req, res) => {
+  res.send('GET PATH 2')
+})
 
 module.exports.handler = serverless(app)
