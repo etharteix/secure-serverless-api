97eb46036cdadafc34eff44e39196b07c9022037
etharteix <59590437+etharteix@users.noreply.github.com>
2023-10-10 20:47:32 -0300

    Removing the API Key validator

--- cloudfront-api/services/cloudfront-api/src/helpers/validator.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = process.env.API_KEY
-
-const validateRoute = (req) => {
-  const requestApiKey = req.apiGateway.event.headers['x-api-key']
-
-  return requestApiKey === API_KEY
-}
-
-module.exports = { validateRoute }
